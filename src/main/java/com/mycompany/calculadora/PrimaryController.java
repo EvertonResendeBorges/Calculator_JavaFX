e78c55b2680c937cbcb4aab615c64d3b6157ecb6
EvertonResendeBorges <77930595+EvertonResendeBorges@users.noreply.github.com>
2023-11-28 17:49:28 -0300

    Calculator JavaFX

---
+++ src/main/java/com/mycompany/calculadora/PrimaryController.java
@@ -0,0 +1,241 @@
+package com.mycompany.calculadora;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+/**
+ * FXML Controller class
+ *
+ * @author evert
+ */
+public class PrimaryController implements Initializable {
+
+    @FXML
+    private TextField result;
+    @FXML
+    private Button n1;
+    @FXML
+    private Button n2;
+    @FXML
+    private Button n3;
+    @FXML
+    private Button n4;
+    @FXML
+    private Button n5;
+    @FXML
+    private Button n6;
+    @FXML
+    private Button n7;
+    @FXML
+    private Button n8;
+    @FXML
+    private Button n9;
+    @FXML
+    private Button n0;
+    @FXML
+    private Button decimal;
+    @FXML
+    private Button add;
+    @FXML
+    private Button subtract;
+    @FXML
+    private Button divide;
+    @FXML
+    private Button multiply;
+    @FXML
+    private Button equal;
+    @FXML
+    private Button Clear;
+    
+    double valor1, valor2, resultado;
+    String calculo;
+    boolean dot = true;
+    boolean newNumber = false;
+  
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+    @FXML
+    private void n1(ActionEvent event) {
+        if(newNumber){
+            result.setText("");
+            newNumber = false;
+        }
+        result.setText( result.getText() + "1");
+        
+    }
+
+    @FXML
+    private void n2(ActionEvent event) {
+        if(newNumber){
+            result.setText("");
+            newNumber = false;
+        }
+        result.setText( result.getText() + "2");
+    }
+
+    @FXML
+    private void n3(ActionEvent event) {
+        if(newNumber){
+            result.setText("");
+            newNumber = false;
+        }
+        result.setText( result.getText() + "3");
+    }
+
+    @FXML
+    private void n4(ActionEvent event) {
+        if(newNumber){
+            result.setText("");
+            newNumber = false;
+        }
+        result.setText( result.getText() + "4");
+    }
+
+    @FXML
+    private void n5(ActionEvent event) {
+        if(newNumber){
+            result.setText("");
+            newNumber = false;
+        }
+        result.setText( result.getText() + "5");
+    }
+
+    @FXML
+    private void n6(ActionEvent event) {
+        if(newNumber){
+            result.setText("");
+            newNumber = false;
+        }
+        result.setText( result.getText() + "6");
+    }
+
+    @FXML
+    private void n7(ActionEvent event) {
+        if(newNumber){
+            result.setText("");
+            newNumber = false;
+        }
+        result.setText( result.getText() + "7");
+    }
+
+    @FXML
+    private void n8(ActionEvent event) {
+        if(newNumber){
+            result.setText("");
+            newNumber = false;
+        }
+        result.setText( result.getText() + "8");
+    }
+
+    @FXML
+    private void n9(ActionEvent event) {
+        if(newNumber){
+            result.setText("");
+            newNumber = false;
+        }
+        result.setText( result.getText() + "9");
+    }
+
+    @FXML
+    private void n0(ActionEvent event) {
+        if(newNumber){
+            result.setText("");
+            newNumber = false;
+        }
+        result.setText( result.getText() + "0");
+    }
+
+    @FXML
+    private void decimal(ActionEvent event) {
+        if(dot){
+            result.setText(result.getText() + ".");
+            dot = false;
+        }
+        
+    }
+
+    @FXML
+    private void add(ActionEvent event) {
+   
+        if(valor1 != 0){
+            valor2 = Double.parseDouble(result.getText());
+            resultado = valor1 + valor2;
+            valor1 = resultado;
+            valor2 = 0;
+            result.setText(Double.toString(resultado));
+            newNumber = true;
+        } else {
+            valor1 = Double.parseDouble(result.getText());
+            result.setText("");
+            calculo = "add";
+            dot = true;
+        }
+    }
+
+    @FXML
+    private void subtract(ActionEvent event) {
+        valor1 = Double.parseDouble(result.getText());
+        result.setText("");
+        calculo = "subtract";
+        dot = true;
+    }
+
+    @FXML
+    private void divide(ActionEvent event) {
+        valor1 = Double.parseDouble(result.getText());
+        result.setText("");
+        calculo = "divide";
+        dot = true;
+    }
+
+    @FXML
+    private void multiply(ActionEvent event) {
+        valor1 = Double.parseDouble(result.getText());
+        result.setText("");
+        calculo = "multiply";
+        dot = true;
+    }
+
+    @FXML
+    private void equal(ActionEvent event) {
+         valor2 = Double.parseDouble(result.getText()); 
+         resultado = valor1 + valor2;
+         
+         switch(calculo){
+             case "add": resultado = valor1 + valor2;
+             break;
+             case "subtract": resultado = valor1 - valor2;
+             break;
+             case "multiply": resultado = valor1 * valor2;
+             break;
+             case "divide": resultado = valor1 / valor2;
+             break;
+         }
+         result.setText(Double.toString(resultado));
+         newNumber = true;
+         valor1 = 0;
+         valor2 = 0;
+    }
+
+    @FXML
+    private void Clear(ActionEvent event) {
+        result.setText("");
+        dot = true;
+        newNumber = true;
+        valor1 = 0;
+        valor2 = 0;
+    }
+
+}
